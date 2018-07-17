565da1d88deab7f674cd5f8d784cf5d908141806
KiruthikaaaPandiyann <41140996+KiruthikaaaPandiyann@users.noreply.github.com>
2018-07-17 08:30:02 -0700

    Create 56a.py

---
+++ 56a.py
@@ -0,0 +1,15 @@
+n=input()
+f1=0
+f2=0
+l=len(n)
+for i in range(0,l):
+    aa=n[i]
+    a=ord(aa)
+    if((a>=65 and a<=90)or(a>=97 and a<=122)):
+        f1=f1+1
+    if(a>=48 and a<=57):
+        f2=f2+1
+if(f1!=0 and f2!=0):
+    print("Yes")
+else:
+    print("No")
